99a70e496593da39867a2e5c4d1743dfa90a8565
raj-neoworks <rajkumar.govindarajan@neoworks.com>
2017-07-25 12:30:16 +0100

    Creating seperate gradle framework for mock services

---
+++ src/main/java/WiremockStarterMain.java
@@ -0,0 +1,65 @@
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
+import com.github.tomakehurst.wiremock.common.FatalStartupException;
+import com.github.tomakehurst.wiremock.common.FileSource;
+import com.github.tomakehurst.wiremock.standalone.CommandLineOptions;
+
+import static com.github.tomakehurst.wiremock.core.WireMockApp.FILES_ROOT;
+import static com.github.tomakehurst.wiremock.core.WireMockApp.MAPPINGS_ROOT;
+import static java.lang.System.out;
+
+public class WiremockStarterMain {
+
+    static {
+        System.setProperty("org.mortbay.log.class", "com.github.tomakehurst.wiremock.jetty.LoggerAdapter");
+    }
+
+    private WireMockServer wireMockServer;
+
+    public static void main(String[] args) {
+        new WiremockStarterMain().run(args);
+    }
+
+    public void run(String... args) {
+        CommandLineOptions options = new CommandLineOptions(args) {
+
+        };
+
+        FileSource fileSource = options.filesRoot();
+        fileSource.createIfNecessary();
+        FileSource filesFileSource = fileSource.child(FILES_ROOT);
+        filesFileSource.createIfNecessary();
+        FileSource mappingsFileSource = fileSource.child(MAPPINGS_ROOT);
+        mappingsFileSource.createIfNecessary();
+
+        wireMockServer = new WireMockServer(options);
+
+        try {
+            wireMockServer.start();
+           // wireMockServer.usingFilesUnderDirectory("/th/to/files-and-mappings-root")
+            out.println(this.getClass().getName());
+            out.println();
+            out.println(options);
+        } catch (FatalStartupException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public static class RequestNotifier extends ConsoleNotifier {
+        public RequestNotifier(boolean verbose) {
+            super(verbose);
+        }
+
+        @Override
+        public void info(String message) {
+            if (message.startsWith("Received request:")) {
+                super.info(message);
+
+            } else if (message.startsWith("Sending response")) {
+                super.info(message.substring(0, Math.min(50, message.length())));
+            }
+        }
+    }
+
+}
---
+++ src/test/java/Common/WireMockClient.java
@@ -0,0 +1,28 @@
+package Common;
+
+/**
+ * Created by rgovindarajan on 20/07/17.
+ */
+
+import com.github.tomakehurst.wiremock.client.HttpAdminClient;
+
+public class WireMockClient {
+
+    //private WireMock wireMock;
+    private static final String WIREMOCK_HOST = "localhost";
+    private static final Integer WIREMOCK_PORT = 8088;
+
+    private static final WireMockClient wiremockClient = new WireMockClient();
+    private static HttpAdminClient wiremockAdminClient = new HttpAdminClient(WIREMOCK_HOST, WIREMOCK_PORT);
+
+    public static WireMockClient getInstance()
+    {
+        return wiremockClient;
+    }
+    public HttpAdminClient getWiremockAdminClient()
+    {
+
+        return wiremockAdminClient;
+
+    }
+}
